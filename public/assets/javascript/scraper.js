76cab464d065e4ad1501ccccc18ea7f377e6ada8
Stephen D'Souza <stephen.roy.dsouza@gmail.com>
2019-10-17 07:59:24 +1100

    add addiional linkfor Articles

--- public/assets/javascript/scraper.js
+++ public/assets/javascript/scraper.js
@@ -3,12 +3,12 @@ $(document).ready(function () {
 
     //On click of sub=mit for burges to be devoured, the id is captured as an object and an ajax call is made .
     //This is passed to burger_controller.js
-    $(".devour-button").on("click", function (event) {
+    $(".scrape").on("click", function (event) {
         var answer = {
             id: $(this).data("newdevour")
         };
 
-        $.ajax("/update", {
+        $.ajax("/scrape", {
                 type: "POST",
                 data: answer
             })
@@ -22,39 +22,4 @@ $(document).ready(function () {
 
     });
 
-    $(".add-burger").on("click", function (event) {
-        var answer = {
-            burger_name: $("#burger").val().trim(),
-        };
-
-
-        $.ajax("/add", {
-                type: "POST",
-                data: answer
-            })
-            //We get the reponse of the answers send reload the page
-            .then(
-                function (response) {
-                    location.reload();
-                }
-            );
-
-
-    });
-    $(".pdf").on("click", function (event) {
-       
-            var id= $(this).data("newdevour")
-        
-        var URL='/pdf/'+id;
-        $.ajax(URL, {
-                type: "GET",
-            })
-            //We get the reponse of the answers send reload the page
-            // .then(
-            //     function (response) {
-            //         // console.log(response);
-
-            //     }
-            // );
-    });
 });
