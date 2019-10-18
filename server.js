46cc183347410efa782fc2cd9a2a840cb6a0f943
Stephen R. D'Souza <stephen.roy.dsouza@gmail.com>
2019-10-18 21:41:55 +1100

    final work done

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/webscraper", { useNewUrlParser: true });
+//mongoose.connect("mongodb://localhost/webscraper", { useNewUrlParser: true });
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/webscraper";
 
 mongoose.connect(MONGODB_URI);
